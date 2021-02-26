43f740c123b03be0ebbd9d505b301e503f90d83c
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-02-26 16:56:59 +0100

    add changes

---
+++ 07-React/React Project - Starter/React Project - TheRightOne/React Project - Starter/src/App-functions.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+
+function App () {
+  const name = 'React projektet'
+  const greeting = 'Välkommen till '
+  const today = new Date().toLocaleDateString()
+
+  return (
+    <div className='App'>
+            
+      <header className='App-header'>
+        <h1>
+          Welcome {greeting} {name}
+        </h1>
+      </header>
+            <p>Today is {today}</p>
+          
+    </div>
+  )
+}
+
+export default App
